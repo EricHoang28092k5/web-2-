04ab9ce46e0ce0ae65dcad4315856092118d080f
tfduongw <nttd482005@gmail.com>
2025-05-06 11:28:34 +0700

    Cap nhat

--- web.sql
+++ web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2025 at 08:05 AM
+-- Generation Time: May 06, 2025 at 06:22 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -38,6 +38,21 @@ CREATE TABLE `chitiethoadon` (
 -- Dumping data for table `chitiethoadon`
 --
 
+INSERT INTO `chitiethoadon` (`IdHoaDon`, `MaSP`, `SoLuong`, `DonGia`) VALUES
+(68, 8, 1, 90000),
+(68, 14, 1, 320000),
+(69, 13, 3, 75000),
+(69, 12, 1, 45000),
+(69, 3, 1, 40000),
+(70, 2, 1, 60000),
+(70, 11, 3, 55000),
+(70, 10, 1, 180000),
+(71, 18, 1, 35000),
+(71, 20, 3, 250000),
+(71, 13, 1, 75000),
+(72, 15, 1, 299000),
+(72, 14, 1, 320000);
+
 -- --------------------------------------------------------
 
 --
@@ -62,6 +77,13 @@ CREATE TABLE `hoadon` (
 --
 -- Dumping data for table `hoadon`
 --
+
+INSERT INTO `hoadon` (`IdHoaDon`, `IdNguoiDung`, `HoTen`, `email`, `sdt`, `DiaChi`, `quan_huyen`, `phuong_xa`, `NgayDatHang`, `TongTien`, `PhuongThucTT`, `TrangThai`) VALUES
+(68, 12, 'Nguyễn Văn A', 'alphabella@gmail.com', '09876543210', '456/12A, Đường Nguyễn Thị Minh Khai, ', 'Quận 3', 'Phường 5', '2025-05-06 10:02:02', 410000, 2, 1),
+(69, 12, 'Nguyễn Văn A', 'alphabella@gmail.com', '09876543210', '1596 Võ Văn Kiệt ', 'Quận 6', 'Phường 7', '2025-05-06 10:05:46', 310000, 1, 1),
+(70, 15, 'Nguyễn Hòa Bình', 'trandao123@gmail', '09390104532', '688/3 Hương Lộ 2 ', 'Quận Bình Tân', 'Phường Bình Trị Đông A', '2025-05-06 10:28:36', 405000, 1, 3),
+(71, 16, 'Võ Hòa Bình', 'binhvo123@gmail.com', '09988776655', '269 Nguyễn Thị Nhỏ ', 'Quận 11', 'Phường 16', '2025-05-06 11:17:30', 860000, 2, 2),
+(72, 16, 'Võ Hòa Bình', 'binhvo123@gmail.com', '09988776655', '269 Nguyễn Thị Nhỏ ', 'Quận 11', 'Phường 16', '2025-05-06 11:17:58', 619000, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -112,11 +134,14 @@ CREATE TABLE `nguoidung` (
 INSERT INTO `nguoidung` (`id_nguoidung`, `tenNguoiDung`, `tenDangNhap`, `email`, `password`, `sdt`, `diaChi`, `quan_huyen`, `phuong_xa`, `vaiTro`, `TrangThai`, `ngay_tao`) VALUES
 (1, 'Nguyễn Tấn Dũng', 'dũng', '123@gmail.com', '202', '01234567899', '88 Lũy Bán Bích', 'Quận 12', 'Tân Thới Hiệp', 'user', 1, '2025-03-31 17:11:55'),
 (3, 'Nguyễn Phạm Phương Trường', 'trường', 'truong01@gmail.com', '123', '01234567891', 'đại học sài gòn', 'Quận 1', 'Bến Thành', 'user', 1, '2025-03-31 17:11:55'),
-(9, 'Hoàng Vũ Minh Mẫn', 'chó mẫn', 'man01@gmail.com', '123', '01234567895', '67 Lê Lợi', 'Quận 1', 'Cầu Kho', 'user', 1, '2025-04-01 10:31:10'),
+(9, 'Hoàng Vũ Minh Mẫn', 'mẫn', 'man01@gmail.com', '123', '01234567895', '67 Lê Lợi', 'Quận 1', 'Cầu Kho', 'user', 1, '2025-04-01 10:31:10'),
 (10, 'Nguyễn Thái Thùy Dương', 'duongw', 'nttd482005@gmail.com', '123', '0902440220', '226 Lý Thường Kiệt', 'Quận 10', 'Phường 14', 'user', 1, '2025-04-06 15:11:24'),
 (11, 'dương', 'dương', 'lalala123@gmail.com', '123', '01234567895', '111 Đường An Dương Vương', 'Huyện Bình Chánh', 'Tân Quý Tây', 'user', 2, '2025-04-16 02:06:54'),
 (12, 'Nguyễn Văn A', 'alphabella', 'alphabella@gmail.com', '123', '09876543210', '456/12A, Đường Nguyễn Thị Minh Khai, ', 'Quận 3', 'Phường 5', 'user', 1, '2025-04-18 14:21:36'),
-(13, 'Cao Phương Nam', 'nam cao', 'namcao123@gmail.com', '123', '09876543210', '123/11 Nguyễn Kiệm', 'Quận Phú Nhuận', 'Phường 3', 'user', 1, '2025-04-24 02:06:25');
+(13, 'Cao Phương Nam', 'nam cao', 'namcao123@gmail.com', '123', '09876543210', '123/11 Nguyễn Kiệm', 'Quận Phú Nhuận', 'Phường 3', 'user', 1, '2025-04-24 02:06:25'),
+(14, 'Nguyễn Tấn Dũng', 'admin', 'dungsinbasento@gmail.com', 'admin123', '09213978392', '56/27 Nguyễn Khuyến', '8', '14', 'admin', 1, '2025-05-06 02:33:18'),
+(15, 'Trần Hưng Đạo', 'daohung', 'trandao123@gmail', '123', '0987665432', '123 nguyễn trãi', 'Quận 5', 'Phường 9', 'user', 1, '2025-05-06 03:25:57'),
+(16, 'Võ Hòa Bình', 'binhvo123', 'binhvo123@gmail.com', '123', '09988776655', '269 Nguyễn Thị Nhỏ ', 'Quận 11', 'Phường 16', 'user', 1, '2025-05-06 04:09:29');
 
 -- --------------------------------------------------------
 
@@ -141,20 +166,24 @@ CREATE TABLE `sanpham` (
 
 INSERT INTO `sanpham` (`MaSP`, `MaLoaiSP`, `TenSP`, `DonGia`, `MoTa`, `HinhAnh`, `TrangThai`, `SoLuongBan`) VALUES
 (1, 1, 'Gà ủ muối', 299000, 'Là một món ăn độc đáo, được chế biến bằng cách ướp thịt gà với muối, các loại gia vị và ủ trong thời gian nhất định. Khi nướng lên, thịt gà sẽ có vị mặn ngọt vừa phải, thịt mềm ngọt và thơm lừng mùi gia vị.', 'ga_u_muoi.jpg', 1, 6),
-(2, 2, 'Rau xào ngũ sắc', 60000, 'Rau xào ngũ sắc là món ăn hấp dẫn với sự kết hợp hài hòa của nhiều loại rau củ có màu sắc bắt mắt như cà rốt, ớt chuông, bông cải, đậu cô ve và nấm. Món ăn không chỉ giàu dinh dưỡng mà còn giòn ngon, đậm đà nhờ cách xào nhanh trên lửa lớn cùng gia vị', 'rau-xao-ngu-sac.jpg', 1, 4),
-(3, 4, 'Trà đào chanh sả', 40000, 'Trà đào chanh sả là một loại đồ uống giải khát thơm ngon, hấp dẫn với sự kết hợp hoàn hảo của vị ngọt thanh từ đào, vị chua dịu của chanh, và hương thơm sả dịu nhẹ. Món đồ uống này không chỉ mang lại cảm giác sảng khoái mà còn rất tốt cho sức khỏe.', 'tra-dao-chanh-sa.jpg', 1, 22),
-(4, 4, 'Nước ép dâu tây', 30000, 'Nước ép dâu tây là thức uống thơm ngon, giàu vitamin C và chất chống oxy hóa, giúp tăng cường sức khỏe và làm đẹp da. Dâu tây tươi được ép cùng một chút đường hoặc mật ong để cân bằng vị chua ngọt', 'nuoc-ep-dau-tay.jpg', 1, 8),
+(2, 2, 'Rau xào ngũ sắc', 60000, 'Rau xào ngũ sắc là món ăn hấp dẫn với sự kết hợp hài hòa của nhiều loại rau củ có màu sắc bắt mắt như cà rốt, ớt chuông, bông cải, đậu cô ve và nấm. Món ăn không chỉ giàu dinh dưỡng mà còn giòn ngon, đậm đà nhờ cách xào nhanh trên lửa lớn cùng gia vị', 'rau-xao-ngu-sac.jpg', 1, 5),
+(3, 4, 'Trà đào chanh sả', 40000, 'Trà đào chanh sả là một loại đồ uống giải khát thơm ngon, hấp dẫn với sự kết hợp hoàn hảo của vị ngọt thanh từ đào, vị chua dịu của chanh, và hương thơm sả dịu nhẹ. Món đồ uống này không chỉ mang lại cảm giác sảng khoái mà còn rất tốt cho sức khỏe.', 'tra-dao-chanh-sa.jpg', 1, 23),
+(4, 4, 'Nước ép dâu tây', 30000, 'Nước ép dâu tây là thức uống thơm ngon, giàu vitamin C và chất chống oxy hóa, giúp tăng cường sức khỏe và làm đẹp da. Dâu tây tươi được ép cùng một chút đường hoặc mật ong để cân bằng vị chua ngọt', 'nuoc-ep-dau-tay.jpg', 0, 8),
 (5, 2, 'Nấm đùi gà cháy tỏi', 69000, 'Nấm đùi gà cháy tỏi là một món ăn đơn giản nhưng vô cùng thơm ngon và bổ dưỡng, rất được ưa chuộng trong các bữa cơm gia đình hoặc làm món ăn nhẹ. Hương vị đặc trưng của nấm giòn ngọt tự nhiên hòa quyện với mùi thơm nồng của tỏi phi ', 'nam-dui-ga-sot-bo-toi.jpg', 1, 1),
 (6, 2, 'Lẩu thái nấm chay', 250000, 'Lẩu Thái nấm chay là một phiên bản thanh đạm nhưng không kém phần đậm đà của món lẩu Thái, rất phù hợp cho những người ăn chay hoặc muốn thưởng thức một món ăn nhẹ nhàng, tốt cho sức khỏe. Với sự kết hợp của nấm tươi, rau củ và các gia vị chua cay, m', 'lau_thai.jpg', 1, 5),
 (7, 1, 'Tôm sú rang bơ tỏi', 310000, 'Tôm sú rang bơ tỏi là một món ăn vô cùng thơm ngon và dễ chế biến, thích hợp cho những bữa ăn gia đình hoặc tiệc tùng. Món ăn này có sự kết hợp hoàn hảo giữa vị ngọt tự nhiên của tôm, bơ béo ngậy và mùi thơm nồng của tỏi phi. Hãy thử ngay', 'tom-su-rang-bo-toi.jpeg', 1, 1),
-(8, 1, 'Cơm chiên cua', 90000, 'Cơm chiên cua là một món ăn thơm ngon, dễ làm và giàu dinh dưỡng, thường xuất hiện trong các bữa ăn gia đình hoặc thực đơn nhà hàng. Món ăn này kết hợp hương vị của cơm chiên vàng ươm, thịt cua ngọt thanh, cùng các loại rau củ tạo nên sự hấp dẫn khó ', 'com_chien_cua.png', 1, 5),
+(8, 1, 'Cơm chiên cua', 90000, 'Cơm chiên cua là một món ăn thơm ngon, dễ làm và giàu dinh dưỡng, thường xuất hiện trong các bữa ăn gia đình hoặc thực đơn nhà hàng. Món ăn này kết hợp hương vị của cơm chiên vàng ươm, thịt cua ngọt thanh, cùng các loại rau củ tạo nên sự hấp dẫn khó ', 'com_chien_cua.png', 1, 6),
 (9, 3, 'Lẩu thái tomyum', 350000, 'Lẩu Thái Tomyum là một món lẩu đặc trưng của ẩm thực Thái Lan, nổi tiếng với hương vị chua cay đậm đà. Nước lẩu được nấu từ nước dùng xương hầm kết hợp với các loại gia vị đặc trưng như sả, riềng, lá chanh Thái, ớt và nước cốt dừa.', 'lau-thai-tomyum.jpeg', 1, 5),
-(10, 1, 'Bò lúc lắc', 180000, 'Bò lúc lắc là món ăn thơm ngon với thịt bò mềm mại, được xào nhanh tay cùng với hành tây, ớt chuông, tạo nên hương vị đậm đà và hấp dẫn.', 'bo-luc-lac.jpg', 1, 7),
-(11, 2, 'Đậu hũ chiên sả ớt', 55000, 'Đậu hũ chiên sả ớt là món chay đơn giản nhưng cực kỳ hấp dẫn với vị giòn bên ngoài, mềm bên trong, thấm đẫm hương sả và vị cay nồng của ớt.', 'dau-hu-chien-sa-ot.jpg', 1, 9),
-(12, 4, 'Trà vải hoa hồng', 45000, 'Trà vải hoa hồng mang đến hương thơm ngọt ngào của vải hòa quyện cùng mùi thơm thanh mát của hoa hồng, là lựa chọn hoàn hảo để giải nhiệt mùa hè.', 'tra-vai-hoa-hong.jpg', 1, 12),
-(13, 2, 'Canh rong biển chay', 75000, 'Canh rong biển chay là món ăn thanh mát, giàu dinh dưỡng với vị ngọt tự nhiên từ rong biển và đậu hũ non, rất thích hợp cho những bữa ăn nhẹ nhàng.', 'canh-rong-bien-chay.jpg', 1, 6),
-(14, 1, 'Cá hồi áp chảo sốt chanh leo', 320000, 'Cá hồi áp chảo sốt chanh leo là món ăn đẳng cấp với cá hồi béo ngậy được áp chảo vàng giòn, kết hợp cùng sốt chanh leo chua ngọt, tạo nên hương vị tinh tế khó cưỡng.', 'ca-hoi-ap-chao-chanh-leo.jpg', 1, 4);
-
+(10, 1, 'Bò lúc lắc', 180000, 'Bò lúc lắc là món ăn thơm ngon với thịt bò mềm mại, được xào nhanh tay cùng với hành tây, ớt chuông, tạo nên hương vị đậm đà và hấp dẫn.', 'bo-luc-lac.jpg', 1, 8),
+(11, 2, 'Đậu hũ chiên sả ớt', 55000, 'Đậu hũ chiên sả ớt là món chay đơn giản nhưng cực kỳ hấp dẫn với vị giòn bên ngoài, mềm bên trong, thấm đẫm hương sả và vị cay nồng của ớt.', 'dau-hu-chien-sa-ot.jpg', 1, 12),
+(12, 4, 'Trà vải hoa hồng', 45000, 'Trà vải hoa hồng mang đến hương thơm ngọt ngào của vải hòa quyện cùng mùi thơm thanh mát của hoa hồng, là lựa chọn hoàn hảo để giải nhiệt mùa hè.', 'tra-vai-hoa-hong.jpg', 1, 13),
+(13, 2, 'Canh rong biển chay', 75000, 'Canh rong biển chay là món ăn thanh mát, giàu dinh dưỡng với vị ngọt tự nhiên từ rong biển và đậu hũ non, rất thích hợp cho những bữa ăn nhẹ nhàng.', 'canh-rong-bien-chay.jpg', 1, 10),
+(14, 1, 'Cá hồi áp chảo sốt chanh leo', 320000, 'Cá hồi áp chảo sốt chanh leo là món ăn đẳng cấp với cá hồi béo ngậy được áp chảo vàng giòn, kết hợp cùng sốt chanh leo chua ngọt, tạo nên hương vị tinh tế khó cưỡng.', 'ca-hoi-ap-chao-chanh-leo.jpg', 1, 6),
+(15, 3, 'Lẩu nấm ', 299000, 'Lẩu nấm, một món ăn thanh đạm nhưng đầy quyến rũ, là sự hòa quyện tinh tế của vô vàn loại nấm tươi ngon cùng nước dùng ngọt thanh, mang đến trải nghiệm ẩm thực khó quên.Lẩu nấm không chỉ là món ăn ngon mà còn rất tốt cho sức khỏe. Nấm là nguồn cung c', 'Lau_nam.jpg', 1, 1),
+(16, 3, 'Lẩu cua đồng', 399000, 'Lẩu cua đồng mang đến một hương vị hài hòa, tinh tế. Vị ngọt thanh, đậm đà của nước dùng quyện cùng vị béo bùi của riêu cua, chút chua nhẹ của cà chua và sự tươi mát của các loại rau. Khi nhúng thêm các loại thịt và hải sản, nồi lẩu càng trở nên phon', 'Lau_cua_dong.jpg', 1, 0),
+(18, 4, 'matcha latte', 35000, 'Thức uống xanh mát, kết hợp hoàn hảo giữa bột trà xanh matcha thượng hạng và sữa tươi mềm mịn. Vị trà thanh tao, hơi ngọt dịu, mang đến cảm giác thư thái và sảng khoái độc đáo, là lựa chọn lý tưởng để khởi đầu ngày mới hoặc thư giãn sau bữa ăn ngon t', 'matcha-latte.jpg', 1, 1),
+(19, 3, 'Lẩu gà', 199000, 'Lẩu gà là một món ăn quen thuộc và được yêu thích trong ẩm thực Việt Nam, đặc biệt là trong những ngày se lạnh hoặc những buổi tụ họp gia đình, bạn bè. Món lẩu này hấp dẫn bởi nước dùng ngọt thanh, thịt gà mềm thơm và sự đa dạng của các loại rau, nấm', 'Lau-ga.jpg', 1, 0),
+(20, 3, 'Lẩu cá chình', 250000, 'Lẩu cá chình mang đến một hương vị độc đáo và hấp dẫn. Thịt cá chình béo ngậy, dai ngon hòa quyện với nước dùng đậm đà, chua cay hoặc ngọt thanh tùy theo cách chế biến. Các loại rau ăn kèm tạo thêm sự cân bằng và tươi mát cho món ăn.', 'lau-ca-chinh.jpg', 1, 3);
 
 --
 -- Indexes for dumped tables
@@ -201,7 +230,7 @@ ALTER TABLE `sanpham`
 -- AUTO_INCREMENT for table `hoadon`
 --
 ALTER TABLE `hoadon`
-  MODIFY `IdHoaDon` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `IdHoaDon` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT for table `loaisanpham`
@@ -213,13 +242,13 @@ ALTER TABLE `loaisanpham`
 -- AUTO_INCREMENT for table `nguoidung`
 --
 ALTER TABLE `nguoidung`
-  MODIFY `id_nguoidung` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_nguoidung` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `sanpham`
 --
 ALTER TABLE `sanpham`
-  MODIFY `MaSP` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `MaSP` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Constraints for dumped tables
@@ -230,7 +259,7 @@ ALTER TABLE `sanpham`
 --
 ALTER TABLE `chitiethoadon`
   ADD CONSTRAINT `chitiethoadon_ibfk_1` FOREIGN KEY (`IdHoaDon`) REFERENCES `hoadon` (`IdHoaDon`),
-  ADD CONSTRAINT `chitiethoadon_ibfk_2` FOREIGN KEY (`MaSP`) REFERENCES `sanpham`(`MaSP`) ON DELETE CASCADE;
+  ADD CONSTRAINT `chitiethoadon_ibfk_2` FOREIGN KEY (`MaSP`) REFERENCES `sanpham` (`MaSP`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `hoadon`
@@ -242,7 +271,7 @@ ALTER TABLE `hoadon`
 -- Constraints for table `sanpham`
 --
 ALTER TABLE `sanpham`
-  ADD CONSTRAINT `sanpham_ibfk_1` FOREIGN KEY (`MaLoaiSP`) REFERENCES `loaisanpham` (`maLoaiSP`);
+  ADD CONSTRAINT `sanpham_ibfk_1` FOREIGN KEY (`MaLoaiSP`) REFERENCES `loaisanpham` (`MaLoaiSP`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
