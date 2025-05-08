8086bb3d0eefbeb71966cd9b026a6fe937f91959
EricHoang28092k5 <manhoang2809@gmail.com>
2025-05-08 14:13:11 +0700

    Thêm favicon và ảnh sản phẩm

--- view/admin/Chinhsuatk.php
+++ view/admin/Chinhsuatk.php
@@ -209,6 +209,11 @@ window.onload = function () {
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link
+      href="../img/DMTD-Food-Logo.jpg"
+      rel="icon"
+      type="image/x-icon"
+    />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
